6cb053e7f548dcd3e3c09254b6315246658021f0
Peter <itspeter87@outlook.com>
2026-06-26 16:25:27 +1000

    Initial commit

---
+++ powershell-scripts/databaseconnection-windowsauth.ps1
@@ -0,0 +1,16 @@
+﻿# Referencing the .NET SDK
+Add-Type -Path "C:\Program Files\Micro Focus\Content Manager\TRIM.SDK.dll"
+
+# Variables such as my TRIM Dataset details
+$wgs = "local"
+$dbId = "45"
+$rtName = "Document"
+
+# Connect to the Database
+
+$db = New-Object TRIM.SDK.Database
+$db.WorkgroupServerName = $wgs
+$db.Id = $dbId
+$db.Connect()
+
+Write-Host $db.Name
