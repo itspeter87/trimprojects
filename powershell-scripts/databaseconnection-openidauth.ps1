885ba61735b281a904e08c422507fba75ee6ce1b
Peter <itspeter87@outlook.com>
2026-06-28 12:31:21 +1000

    Update

---
+++ powershell-scripts/databaseconnection-openidauth.ps1
@@ -0,0 +1,19 @@
+﻿# Referencing the .NET SDK
+Add-Type -Path "C:\Program Files\Micro Focus\Content Manager\TRIM.SDK.dll"
+
+# Variables such as my TRIM Dataset details
+$wgs = "local"
+$dbId = "45"
+
+# Connect to the Database
+$db = New-Object TRIM.SDK.Database
+
+# Tell it to connect via Open ID Authentication and that the app has a user interface to enter the credentials, for example: Microsoft Entra login details
+$db.AuthenticationMethod = [TRIM.SDK.ClientAuthenticationMechanism]::OpenId
+[TRIM.SDK.TrimApplication]::HasUserInterface = $true
+#NOTE: If connecting via https use $db.WorkgroupServerURL rather than $db.WorkgroupServerName
+$db.WorkgroupServerName = $wgs
+$db.Id = $dbId
+$db.Connect()
+
+Write-Host $db.Name
---
+++ powershell-scripts/updatedatelogin-foropenid.ps1
@@ -0,0 +1,30 @@
+﻿
+
+Add-Type -Path "C:\Program Files\Micro Focus\Content Manager\TRIM.SDK.dll"
+
+# Variables such as my TRIM Dataset details
+$wgs = "local"
+$dbId = "45"
+
+# Connect to the Database
+
+$db = New-Object TRIM.SDK.Database
+$db.WorkgroupServerName = $wgs
+$db.Id = $dbId
+$db.Connect()
+
+# Update the network login
+
+$locSearch = New-Object TRIM.SDK.TrimMainObjectSearch
+$locSearch.SetSearchString()
+
+foreach($location in $locSearch)
+{
+  # Get the SMTP email address
+
+  
+
+}
+
+
+
